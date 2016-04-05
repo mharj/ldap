0c8d24c8585e782f3729ce85bcb8aadbed5b40dd
Marko Harjula <marko.harjula@gmail.com>
2016-04-05 10:53:08 +0300

    Update LdapEntry.php

--- src/LdapEntry.php
+++ src/LdapEntry.php
@@ -2,5 +2,32 @@
 namespace mharj;
 
 class LdapEntry extends \stdClass {
-
+  private static $dropInternalAttrs = array('createtimestamp','creatorsname','modifytimestamp','modifiersname');
+  
+  public function equals(LdapEntry $entry) {
+    $target = $this->sort($this->dropInternal( (array)$entry ) );
+    $source = $this->sort($this->dropInternal( (array)$this ) );
+    return false;
+  }
+  
+  private function dropInternal(array $a) {
+    $ret = $a;
+    foreach ( LdapEntry::$dropInternalAttrs AS $attr ) {
+      if ( isset($ret[$attr]) ) {
+        unset($ret[$attr])
+      }
+    }
+    return $ret;
+  }
+  
+  private function sort(array $a) {
+    $ret = $a;
+    ksort($ret);
+    foreach ( $ret AS $k => $v ) {
+      if ( is_array($v) ) {
+        sort($v);
+        $ret[$k]=$v;
+      }
+    }
+  }
 }
